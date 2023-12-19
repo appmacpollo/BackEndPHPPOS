042782f46841422e18b72146e7f387ca6eaa33ce
Jaime Arias <andres-av@hotmail.es>
2023-12-19 09:23:10 -0500

    v1. metodo de validar Caja y consultar producto interno ingreso del metodo de validar caja para validar que informacion tiene metodo de consultar productos interno metodo de calcular valores y existencias del producto

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -9,7 +9,7 @@ class ProductoController extends Controller
 {
     public function ConsultarProducto($ean)
     {
-        $grupoPrecios = '20';
+        $grupoPrecios = config('app.grupoPrecios');
         $codigoBarras = $ean;
 
         $producto = DB::select('SELECT top 1 p.Producto producto, pr.UnidadMedidaVenta unidad, p.Nombre nombre,'
@@ -72,6 +72,45 @@ class ProductoController extends Controller
 
     public function ValidarCaja()
     {
+        //Validar que no este en la ventana de cierre
+        $Ventanas = DB::select(" SELECT * FROM Ventanas where Opcion in ('INF_CIPRO','INV_FIS') ");
+        if(count($Ventanas) >= 1)
+        {
+            return response()->json([
+                'status' => false,
+                'message' => "No es posible realizar venta. Caja principal se encuentra en Cierre, Inventario Fisico.",
+            ], 200);
+        }
 
+        return response()->json([
+            'status' => true,
+            'message' => "",
+        ], 200);
     }
+
+    public function ConsultarProductoInterno($codId)
+    {
+        $grupoPrecios = config('app.grupoPrecios');
+        $producto = DB::select('SELECT top 1 p.Producto producto, pr.UnidadMedidaVenta unidad, p.Nombre nombre,'
+            . 'isnull(p.PesoPromedio, 0) pesoPromedio, pr.Precio precio, p.ValorImpuesto impuesto,'
+            . 'p.Pesado pesado, isnull(p.PesoMinimo, 0) pesoMinimo,'
+            . 'isnull(p.PesoMaximo, 0) pesoMaximo, p.ToleranciaMinima tolMinima,'
+            . 'p.ToleranciaMaxima tolMaxima, isnull(p.Existencias, 0) existencias,'
+            . "(select case when count(Componente) = 0 then '' else 'X' end Com from Combos where Combo = p.Producto and Estado = 'A') combo, "
+            . "isnull(p.ExistenciasK, 0) existenciasK, ValorImpUltraprocesado as impProcesado,'' oferta "
+            . 'FROM productos p INNER JOIN Precios pr on p.Producto = pr.Producto'
+            . " WHERE pr.GrupoPrecios = '$grupoPrecios' and p.producto = '$codId' "
+            . " and pr.Estado = 'A' and p.Estado = 'A' and p.UnidadMedidaBase = 'S' and pr.Precio > 0 ");
+
+        $descuento = DB::select('SELECT top 1 valor,ClaseDescuento,TipoDescuento '
+            . 'FROM Descuentos '
+            . 'inner join productos on productos.producto = Descuentos.producto '
+            . "where productos.producto = '$codId' and GrupoPrecios = '$grupoPrecios' and Descuentos.Estado = 'A' "
+            . 'and (select FechaProceso from Parametros ) between fechaInicio and fechaFin '
+            . 'order by ClaseDescuento desc');
+  
+        return $productos = array("Producto" => $producto, "Descuento" => $descuento);
+    }
+
+    
 }
