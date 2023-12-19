e4832ee79de6354365de639f0fc3924c49cc759a
Jaime Arias <andres-av@hotmail.es>
2023-12-19 09:23:45 -0500

    v1. metodos de consumo de la api

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductoController;
 use App\Http\Controllers\ImprimirController;
 use App\Http\Controllers\DatafonoController;
+use App\Http\Controllers\FacturaController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,6 +21,9 @@ use App\Http\Controllers\DatafonoController;
 
 Route::get('/ConsultarProducto/{ean}', [ProductoController::class, 'ConsultarProducto']);
 Route::get('/ImprimirFactura/{factura}/{clase}/{prefijo}/{maquina}', [ImprimirController::class, 'ImprimirFactura']);
+Route::get('/ValidarVentas', [ProductoController::class, 'ValidarCaja']);
+Route::post('/ValorPago', [FacturaController::class, 'ValorPago']);
+Route::post('/facturar', [FacturaController::class, 'facturar']);
 
 Route::get('/EnviarADatafono/{valorTotal}/{valorImpuestos}/{abreviatura}', [DatafonoController::class, 'EnviarADatafono']);
 Route::get('/leerArchivoSalidaDatafono', [DatafonoController::class, 'SalidaDatafono']);
